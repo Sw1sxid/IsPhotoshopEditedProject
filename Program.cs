f502a6639f63e706415c96d3f15558703fe3b608
Sw1sxid <lisov_03@mail.ru>
2023-11-30 12:09:25 +0300

    Изменения в коде включают использование новой библиотеки GroupDocs.Metadata для работы с метаданными изображений, а также добавление логики для отслеживания изменений и проверки наличия строки "Adobe Photoshop" в значениях свойств изображения. Эти изменения позволяют определить, было ли изображение отредактировано с помощью программы Adobe Photoshop.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using GroupDocs.Metadata.Standards.Exif;
 using GroupDocs.Metadata;
+using GroupDocs.Metadata.Formats.Image;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 class Program
 {
@@ -7,36 +10,63 @@ class Program
     {
         // Получение путя к папке и получение всех изображений внутри
         string folderPath = @".\img";
-        string[] files = Directory.GetFiles(folderPath);
+        string[] files = System.IO.Directory.GetFiles(folderPath);
 
-        //Проверяем каждое изображение
+        //Проверяем каждое изображение 
         foreach (string file in files)
         {
-            IsPhotoEdited(file);
+            Console.WriteLine(file + " Фото было отредактировано: " + IsPhotoEdited(file));
         }
+        Console.ReadLine();
     }
     static bool IsPhotoEdited(string filePath)
     {
-        // Используем библиотеку GroupDocs.Metadata для работы с метаданными изображения
-        using (Metadata metadata = new Metadata(filePath))
+        // Переменная edited используется для отслеживания того, редактирован ли изображение.
+        bool edited = false;
+
+        // Создание объекта Bitmap для представления изображения.
+        Bitmap image = new Bitmap(filePath);
+
+        // Получение массива PropertyItem, содержащего метаданные изображения.
+        PropertyItem[] propItems = image.PropertyItems;
+
+        foreach (PropertyItem item in propItems)
         {
-            // Получаем корневой пакет (IExif) из метаданных изображения
-            IExif root = metadata.GetRootPackage() as IExif;
+            // Получение числового идентификатора свойства в формате short.
+            short id = short.Parse(item.Id.ToString("x"), System.Globalization.NumberStyles.HexNumber);
 
-            // Если корневой пакет или его ExifPackage равны null, то изображение считается измененным
-            if (root == null || root.ExifPackage == null)
-            {
-                Console.WriteLine("Image edited: " + filePath + "\n");
-                return true;
-            }
+            // Получение значения свойства в виде строки с использованием метода GetValueString.
+            string value = GetValueString(item.Type, item.Value);
 
-            // Если корневой пакет и его ExifPackage не равны null, то изображение считается неизмененным
-            else
+            switch (id)
             {
-                Console.WriteLine("Image is not edited: " + filePath + "\n");
-                return false;
+                case 0x131: // Идентификатор свойства "ImageSoftwareVersion" в формате EXIF.
+                            // Проверка, начинается ли значение с "Adobe Photoshop".
+                    if (value.StartsWith("Adobe Photoshop"))
+                    {
+                        // Изображение редактировано.
+                        edited = true;
+
+                        break;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
+
+        // Возврат значения переменной edited, указывающего на то, редактирован ли изображение.
+        return edited;
     }
 
+    static string GetValueString(short type, byte[] value)
+    {
+        // Логика преобразования значений в читаемый формат
+        if (type == 2) // ASCII
+        {
+            return System.Text.Encoding.ASCII.GetString(value);
+        }
+
+        return BitConverter.ToString(value);
+    }
 }
